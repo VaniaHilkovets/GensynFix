a74e47b5eca3dfcb5896b09e75ab107f3b5fab1a
VaniaHilkovets <32434641+VaniaHilkovets@users.noreply.github.com>
2025-04-04 13:13:22 +0300

    run_rl_swarm.sh

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -13,6 +13,9 @@ DEFAULT_HOST_MULTI_ADDRS="/ip4/0.0.0.0/tcp/38331"
 PEER_MULTI_ADDRS=${PEER_MULTI_ADDRS:-$DEFAULT_PEER_MULTI_ADDRS}
 HOST_MULTI_ADDRS=${HOST_MULTI_ADDRS:-$DEFAULT_HOST_MULTI_ADDRS}
 
+# Установка зависимостей
+apt update && apt install -y python3 python3-pip curl git sudo jq wget lsof cloudflared
+
 # Спрашиваем про подключение к Testnet
 read -p $'\e[1;36mПодключиться к Testnet? [Y/n]: \e[0m' yn
 yn=${yn:-Y}
@@ -23,31 +26,29 @@ else
 fi
 
 if [[ "$CONNECT_TO_TESTNET" == "True" ]]; then
-    echo -e "\n\e[1;33m[1/4] Устанавливаем Node.js 20 и npm (если не установлены)...\e[0m"
+    echo -e "\n\e[1;33m[1/4] Устанавливаем Node.js 20 и npm (esli ne ustanovleny)...\e[0m"
     if ! command -v node >/dev/null 2>&1; then
         curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
         sudo apt-get install -y nodejs
     fi
 
-    echo -e "\n\e[1;33m[2/4] Устанавливаем зависимости...\e[0m"
+    echo -e "\n\e[1;33m[2/4] Установка npm-зависимостей...\e[0m"
     cd "$ROOT/modal-login"
-
     rm -rf node_modules package-lock.json yarn.lock
-
     npm install --legacy-peer-deps
 
-    echo -e "\n\e[1;33m[3/4] Патчим проблему с sonic...\e[0m"
+    echo -e "\n\e[1;33m[3/4] Патчим sonic импорт...\e[0m"
     sed -i '/import.*sonic/d' node_modules/@account-kit/react/node_modules/@account-kit/infra/dist/esm/chains.js || true
 
-    echo -e "\n\e[1;33m[4/4] Запускаем dev сервер...\e[0m"
+    echo -e "\n\e[1;33m[4/4] Запускаем dev-сервер...\e[0m"
     npm run dev > "$ROOT/server.log" 2>&1 &
     SERVER_PID=$!
     cd "$ROOT"
 
-    echo -e "\n\e[1;36mОткрой новое окно и выполни:\e[0m"
+    echo -e "\n\e[1;36mОткрой новое окно и введи:\e[0m"
     echo -e "\e[1;32mcloudflared tunnel --url http://localhost:3000\e[0m"
 
-    echo -e "\nОжидаем появления userData.json..."
+    echo -e "\nОжидаем userData.json..."
     while [ ! -f "modal-login/temp-data/userData.json" ]; do
         sleep 3
         echo "Waiting..."
@@ -57,7 +58,7 @@ if [[ "$CONNECT_TO_TESTNET" == "True" ]]; then
     ORG_ID=$(awk 'BEGIN { FS = "\"" } !/^[ \t]*[{}]/ { print $(NF - 1); exit }' modal-login/temp-data/userData.json)
 fi
 
-# Ставим питон зависимости
+# Python dependencies
 pip install -r "$ROOT/requirements-hivemind.txt" > /dev/null
 pip install -r "$ROOT/requirements.txt" > /dev/null
 
@@ -68,7 +69,7 @@ else
     CONFIG_PATH="$ROOT/hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml"
 fi
 
-# Хаб токен
+# HF Token
 if [ -n "$HF_TOKEN" ]; then
     HUGGINGFACE_ACCESS_TOKEN=$HF_TOKEN
 else
@@ -81,7 +82,7 @@ else
     fi
 fi
 
-# Запуск
+# Launch
 echo -e "\n\e[1;35mЗапуск ноды...\e[0m"
 if [ -n "$ORG_ID" ]; then
     python -m hivemind_exp.gsm8k.train_single_gpu \
